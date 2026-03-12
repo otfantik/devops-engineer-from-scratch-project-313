402fd89ead2fc48e4fa59525ff84c73556d36e59
Sergey Mazov <sergeymazov@Sergeys-MacBook-Air-2.local>
2026-03-12 15:58:56 +0300

    ruff

--- app/main.py
+++ app/main.py
@@ -1,25 +1,30 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
+
 from app.database import create_db_and_tables
 from app.routers import links
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
+
 app = FastAPI(title="DevOps Engineer Project", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(links.router, prefix="/api/links", tags=["links"])
+
 
 @app.get("/ping")
 async def ping():
